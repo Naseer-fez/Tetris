91b5233369e56098fec310ec3ca7d32b9af3beb4
Naseer <feznaseer777@gmail.com>
2026-05-21 13:33:29 +0530

    Removed The Repetive 2d char for using the 2d int solution and removed the bordermaker function for efficency

--- Tetris.c
+++ Tetris.c
@@ -1,98 +1,184 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <time.h>
+#include "Allimports.h"
 #include "Shapes.h"
-#include 
+#include "Functions.h"
 /*
-To compile 
-gcc Tetris.c shapes.c -o Tetris
-.\Tetris
+To compile
+ gcc Tetris.c Shapes.c Functions.c -o Tetris; if ($?) { .\Tetris }
 
 */
 
 // ***You Shoudl change the Left variabe not the rigth variable
-#define Length (19 + 1)
-#define Height (19 + 1)
+#define Length (10 + 1)
+#define Height (10 + 1)
 
-char grid[Height][Length];
-
-
+// char grid[Height][Length];
+int Postion[Height][Length];
 void GridPrinting();
 void BorderMaker();
-void blocksCreation();
-char *BlockMaker(int BlockValue);
-
+void BlockPlacement(int HorPlacement, int virPlacement);
+bool isvalid(int *block, int len, int hor, int ver);
+// char *BlockMaker(int BlockValue);
 
 typedef struct
 {
-    int horsides;
-    int versides;
-    int closed;
+    unsigned char Horizontal;
+    unsigned char Vertical;
 
-} Block;
+} Side;
+
+Side Directions[Length - 2];
+// Start from 1 and go till Length -2 for the values
+void timepass()
+{
+
+    for (int i = 0; i < Height; i++)
+    {
+        printf("%d::", i + 1);
+        for (int j = 0; j < Length; j++)
+        {
+            printf("%d\t", Postion[i][j]);
+        }
+        printf("\n");
+    }
+    sleep(3);
+    system("cls");
+}
 
 int main()
 {
-
     BorderMaker();
-    int c = 0;
-
+    BlockPlacement(-1, -1);
+    // timepass();
     GridPrinting();
 }
 
+// void BorderMaker()
+// {
+//     for (int i = 0; i < Length - 2; i++)
+//     {
+//         Directions[i].Horizontal = Length - 2;
+//         Directions[i].Vertical = Height - 2;
+//     }
 
-void BorderMaker()
+//     for (int row = 0; row < Height; row++)
+//     {
+//         for (int col = 0; col < Length; col++)
+//         {
+//             if ((row == 0) || (row == Height - 1))
+//             {
+//                 grid[row][col] = '-';
+//                 Postion[row][col] = -1;
+//             }
+//             else if ((col == 0) || (col == Length - 1))
+//             {
+//                 grid[row][col] = '|';
+//                 Postion[row][col] = -1;
+//             }
+//             else
+//             {
+//                 grid[row][col] = '.';
+//                 Postion[row][col] = 0;
+//             }
+//         }
+//     }
+// }
+
+bool isvalid(int *block, int len, int hor, int ver)
 {
-
-    for (int row = 0; row < Height; row++)
+    // Directions[hor];
+    for (int i = 0; i < len; i++)
     {
-        for (int col = 0; col < Length; col++)
+        if (block[i] == 1)
         {
-            if ((row == 0) || (row == Height - 1))
+
+            int row = ver;
+            int col = hor + i;
+            if (row < 0 || row >= Height || col < 0 || col >= Length)
             {
-                grid[row][col] = '-';
-                Horsize++;
+                return false;
             }
-            else if ((col == 0) || (col == Length - 1))
+            if (Postion[row][col] == -1 || Postion[row][col] == 2)
             {
-                grid[row][col] = '|';
-                Lensize++;
-            }
-            else
-            {
-                grid[row][col] = '.';
-                check++;
+
+                return false;
             }
         }
     }
+    return true;
 }
 
-char *BlockMaker(int BlockValue)
+void BlockPlacement(int HorPlacement, int virPlacement)
 {
-    char *block = (char *)malloc(sizeof(char) * 200);
-    block[0] = '\0';
-    int row = sizeof(Shapes[BlockValue]) / sizeof(Shapes[BlockValue][0]);
-    int col = sizeof(Shapes[BlockValue][0]) / sizeof(Shapes[BlockValue][0][0]);
-    for (int i = 0; i < row; i++)
+
+    // This zero have to be replace with randoem value gen
+    char *Block = BlockMaker(0);
+    if (HorPlacement == -1 && virPlacement == -1)
     {
-        for (int j = 0; j < col; j++)
-        {
-            int index = (Shapes[BlockValue][i][j] == 1) ? 1 : 0;
-            strcat(block, SideValue[index]);
-        }
-        strcat(block, "\n");
+        HorPlacement = Length / 2;
+        virPlacement = 1;
     }
-    return block;
+    int *demcount = ShapeSender(0, -1); // 0 is row , 1 is col
+
+    for (int i = 0; i < demcount[0]; i++)
+    {
+        int *check = ShapeSender(0, i);
+        int len = sizeof(check) / sizeof(int);
+        if (!(isvalid(check, len, HorPlacement, virPlacement)))
+        {
+            // system("cls");
+            fprintf(stderr, "Sorry Game Over\n");
+            exit(0);
+        }
+
+        for (int j = 0; j < len; j++)
+        {
+            Postion[virPlacement][HorPlacement + j] = check[j];
+        }
+        virPlacement++;
+    }
 }
 
+// void GridPrinting()
+// {
+//     for (int i = 0; i < Height; i++)
+//     {
+//         for (int j = 0; j < Length; j++)
+//         {
+//             printf("%c", grid[i][j]);
+//         }
+//         printf("\n");
+//     }
+// }
 void GridPrinting()
 {
     for (int i = 0; i < Height; i++)
     {
         for (int j = 0; j < Length; j++)
         {
-            printf("%c", grid[i][j]);
+            int value=Postion[i][j];
+            if ( value== -1)
+            {
+               if (i == 0 || i == Height - 1)
+                {
+                    printf("-");
+                }
+                else
+                {
+                    printf("|");
+                }
+                
+            }else{
+            if (value==0){
+                printf(".");
+            }
+            else if (value==-2){
+                printf("#");
+            }
+            else if (value==1){
+                printf("$");
+            }
+        }
+
         }
         printf("\n");
     }
